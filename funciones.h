6e1788f1703a0d73f510b60d967225f06e238b92
TomasAlvarez20 <92554936+TomasAlvarez20@users.noreply.github.com>
2021-11-28 22:29:48 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,31 +1,16 @@
-int sumar(int a, int b){
-    return a + b;
-}
+#ifndef FUNCIONES_H_INCLUDED
+#define FUNCIONES_H_INCLUDED
 
-float restar(float a, float b){
-    return a - b;
-}
+int sumar(int a, int b);
 
-float multiplicar(float a, float b){
-    return a * b;
-}
+int restar(int a, int b);
 
-float dividir(float a, float b){ 
-    if (a > 0 && b == 0){
-        return -1;
-    }
-    return a / b;
-}
+int multiplicar(int a, int b);
 
-int calcularFactorial(int a)
-{
-    int factorial;
-    if (a == 1 || a == 0)
-    {
-        //printf("1 -> 1 \n");
-        return 1;
-    }
-    factorial = a * calcularFactorial(a - 1);
-    //printf("%d -> %d \n",a ,result);
-    return factorial;
-}
+int dividir(int a, int b);
+
+int calcularFactorial(int a);
+
+int validarNumero(char string[]);
+
+#endif // FUNCIONES_H_INCLUDED
